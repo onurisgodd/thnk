0530b24da3a93de4ac35425caaec49248a06d242
onurisgodd <74376709+onurisgodd@users.noreply.github.com>
2023-07-31 15:33:39 -0700

    Create executefile.go

---
+++ executefile.go
@@ -0,0 +1,47 @@
+package executefile
+
+
+import (
+	"fmt"
+	"github.com/tiagorlampert/CHAOS/client/app/environment"
+	"github.com/tiagorlampert/CHAOS/client/app/gateways"
+	"github.com/tiagorlampert/CHAOS/client/app/services"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type Service struct {
+	Configuration *environment.Configuration
+	Gateway       gateways.Gateway
+}
+
+func NewService(configuration *environment.Configuration, gateway gateways.Gateway) *Service {
+	return &Service{
+		Configuration: configuration,
+		Gateway:       gateway,
+	}
+}
+
+func (s *Service) RunFile(filepath string) error {
+	filename := getFilenameFromPath(filepath)
+	url := fmt.Sprintf("%s/%s", fmt.Sprint(s.Configuration.Server.Url, "execute"), filename)
+
+	res, err := s.Gateway.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("request failed with status code: %d", res.StatusCode)
+	}
+
+	if err := ioutil.WriteFile(filepath, res.ResponseBody, os.ModePerm); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getFilenameFromPath(path string) string {
+	return filepath.Base(path)
+}
